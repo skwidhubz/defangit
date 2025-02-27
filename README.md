dbf71618e43741a0a93aec1c2e97f55c9e17c02c
skwidhubz <tommaxwellparker@gmail.com>
2025-02-27 22:09:00 +1100

    fang commitit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # defangit
-De-Fang spicy strings
+De-Fang spicy strings!
+
+How to run: 
+
+node defang.js spicystring.example.com
+
+
+
+:: made by lordradias 4 u ::
---
+++ defang.js
@@ -0,0 +1,23 @@
+// Program to de-fang URLS/IPs
+// e.g convert www.google.com to www(.)google(.)com
+
+console.log("deFangIT o_O");
+
+function defangit(fangstring) {
+	return fangstring.split('.').join('[.]');
+};
+
+const args = process.argv.slice(2);
+const fangedString = args[0];
+
+if (!fangedString) {
+	console.log("please provide a spicy string...");
+} else {
+	let deFangedString = defangit(fangedString);
+	console.log("De-fanged:   ", deFangedString);
+};
+
+// s*** code but 
+
+
+
